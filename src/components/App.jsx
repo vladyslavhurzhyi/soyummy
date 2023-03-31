0c8084e97d57415aacece6b3957a597c52ebb19a
IreneCreadora <nargiza.dancer@gmail.com>
2023-03-31 14:29:22 +0100

    added btn and section title template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div className="flex justify-center mt-8">
+      <h1 className="section-title">
+        Categories Add recipe Favorites My recipes Search Shopping list
+      </h1>
+      <button className="main-btn">Search</button>
+      <button className="custom-btn">Search</button>
     </div>
   );
 };
