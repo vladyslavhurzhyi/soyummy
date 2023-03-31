0951f2c04aee8c8e5d73465dc97ede243135103b
IreneCreadora <nargiza.dancer@gmail.com>
2023-03-31 14:29:01 +0100

    added extend colors, fonts, ets..

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accentMain: '#8BAA36',
+        accentLighter: '#EBF3D4',
+        accentDark: '#22252A',
+        accentDarker: '#1E1F28',
+        accentHalfDark: '#2A2C36',
+        accentGray: '#D9D9D9',
+        mainText: '#001833',
+        secondaryText: '#3E4462',
+      },
+      spacing: {
+        '52px': '52px',
+      },
+      fontFamily: {
+        main: ['Poppins'],
+      },
+      fontSize: {
+        customXxs: ['14px', '14px'],
+        customXs: ['14px', '21px'],
+        customSm: ['16px', '20px'],
+        customBase: ['24px', '24px'],
+        customLg: ['28px', '28px'],
+        customXl: ['44px', '44px'],
+      },
+    },
   },
   plugins: [],
 };
