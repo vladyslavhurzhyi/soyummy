57dded0d0fd5e85e742137671d1a19b7798db23d
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2023-04-01 15:23:33 +0300

    Added welcomePage import to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 import { Home } from 'pages/Home/Home';
+import { Register } from 'pages/Register/Register';
+import { Login } from 'pages/Login/Login';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/signin" element={<Login />} />
       </Route>
     </Routes>
   );
