eb1389568b4332fe892565e6f424ffd5dffa9c0a
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2023-04-01 17:33:12 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,11 +11,12 @@ const WellcomPage = lazy(() => import('components/WellcomPage/WellcomPage'));
 
 export const App = () => {
   return (
+    <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/add" element={<AddRecipePage />} />
         <Route path="/myrecipes" element={<MyRecipesList />} />
-         <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/signin" element={<Lognin />} />
         <Route path="/welcomePage" element={<WellcomPage />} />
       </Route>
