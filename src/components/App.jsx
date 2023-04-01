108402a3f66f2d0447b3b485899f0fd9dd88348b
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2023-04-01 17:28:40 +0300

    Update App.jsx add lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,15 @@ import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 import { MyRecipesList } from './MyRecipesList/MyRecipesList';
-import { Register } from 'pages/RegisterPage/RegisterPage';
-import { Lognin } from 'pages/LogninPage/LogninPage';
-import { WellcomPage } from 'components/WellcomPage/WellcomPage';
 
 const HomePage = lazy(() => import('../pages/Home'));
 const AddRecipePage = lazy(() => import('../pages/AddRecipe'));
+const Register = lazy(() => import('../pages/RegisterPage/RegisterPage'));
+const Lognin = lazy(() => import('../pages/LogninPage/LogninPage'));
+const WellcomPage = lazy(() => import('components/WellcomPage/WellcomPage'));
 
 export const App = () => {
   return (
-   <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/add" element={<AddRecipePage />} />
