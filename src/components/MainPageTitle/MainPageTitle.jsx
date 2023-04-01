318b95fe1a4e94588a67259b5d1bbf5b6ebcf0f3
Serhii Malychkovych <masevik@gmail.com>
2023-04-01 17:35:52 +0300

    add MainPageTitle with styles

---
+++ src/components/MainPageTitle/MainPageTitle.jsx
@@ -0,0 +1,7 @@
+// possible classes:
+// section-title
+// section-title-accent
+
+export const MainPageTitle = ({ pageTitle, className }) => {
+  return <h1 className={className}>{pageTitle}</h1>;
+};
