80e8ea64dc7fa70d82a14c953e8bb1bfacf756fe
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2023-04-02 02:27:14 +0300

    Add redux> auth: Operation, Selector, Slice; service: routes and API: auth user . Add route in app.js

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -1,2 +1,11 @@
-// export const getLoges = state => state.auth.isLoggedIn;
-// export const getUserFetching = state.auth.isUserFetching;
+//---TOKEN---
+export const getAccessToken = state => state.auth.accessToken;
+
+//---USER---
+export const getEmail = state => state.auth.userData.email;
+export const getName = state => state.auth.userData.name;
+export const getAvatar = state => state.auth.userData.avatar;
+export const getToken = state => state.auth.accessToken;
+//---ADDITIONALLY
+export const getLoges = state => state.auth.Login;
+export const getUserFetching = state => state.auth.userFetching;
