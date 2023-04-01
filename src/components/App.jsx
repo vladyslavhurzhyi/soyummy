080d8af206bc904496a0519da10a534ae4b2af0f
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-04-01 15:45:20 +0300

    add simple layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 import { Home } from 'pages/Home/Home';
+import { MyRecipesList } from './MyRecipesList/MyRecipesList';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
+        <Route path="/myrecipes" element={<MyRecipesList />} />
       </Route>
     </Routes>
   );
