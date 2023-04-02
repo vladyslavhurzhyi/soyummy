7f2b6f0176399a046b6d916905ddb41dd98cd405
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2023-04-02 15:50:28 +0300

    Made the login button transparent

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -10,7 +10,7 @@ export const AuthNav = () => {
         Registration
       </NavLink>
       <NavLink
-        className="transition duration-300 py-2 px-5 ml-2  rounded-tr-3xl rounded-br-lg rounded-bl-3xl rounded-tl-lg border font-main text-zinc-50 text-base bg-accentMain hover:bg-accentDark  focus:ring"
+        className="transition duration-300 py-2 px-5 ml-2  rounded-tr-3xl rounded-br-lg rounded-bl-3xl rounded-tl-lg border font-main text-zinc-50 text-base bg-transporent hover:bg-accentDark  focus:ring"
         to="/signin"
       >
         Log In
