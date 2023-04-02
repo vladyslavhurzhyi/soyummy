fcf16ebb118039ab720777fb8173ce435ce5c7ab
Vladyslav Hurzhyi <80459886+vladyslavhurzhyi@users.noreply.github.com>
2023-04-02 23:38:48 +0200

    Update ChooseYourBreakfast.jsx

--- src/components/ChooseYourBreakfest/ChooseYourBreakfast.jsx
+++ src/components/ChooseYourBreakfest/ChooseYourBreakfast.jsx
@@ -1,26 +1,26 @@
-export const ChooseYourBreakfast = () => {
-  return (
-    <div>
-      <p>
-        <span>Delicious and healthy</span>
-        way to enjoy a variety of fresh ingredients in one satisfying meal
-      </p>
+// export const ChooseYourBreakfast = () => {
+//   return (
+//     <div>
+//       <p>
+//         <span>Delicious and healthy</span>
+//         way to enjoy a variety of fresh ingredients in one satisfying meal
+//       </p>
 //       <ChooseLink to="/categories/breakfast">
-        See recipes{' '}
-        <svg
-          class="w-4 h-3 ml-1 flex"
-          viewBox="0 0 15 12"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M1.5 6H13.5M13.5 6L9 1.5M13.5 6L9 10.5"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
+//         See recipes{' '}
+//         <svg
+//           class="w-4 h-3 ml-1 flex"
+//           viewBox="0 0 15 12"
+//           fill="none"
+//           xmlns="http://www.w3.org/2000/svg"
+//         >
+//           <path
+//             d="M1.5 6H13.5M13.5 6L9 1.5M13.5 6L9 10.5"
+//             strokeWidth="1.5"
+//             strokeLinecap="round"
+//             strokeLinejoin="round"
+//           />
+//         </svg>
 //       </ChooseLink>
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
