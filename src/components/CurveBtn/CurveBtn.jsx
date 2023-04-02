4889764babed9cf1314ce6ce1c08f6f56283d9ff
Vladyslav Hurzhyi <soulline0@gmail.com>
2023-04-02 02:17:19 +0200

    add btn

---
+++ src/components/CurveBtn/CurveBtn.jsx
@@ -0,0 +1,18 @@
+//registration-btn --- class for curve btn registration + style  borderRadius: '9% 23% 11% 21% / 26% 57% 34% 60% '
+//signin-btn --- class for curve btn welcome page bg-transparent
+//searchbl-btn --- class for curve search btn (black bg)
+//searchgreen-btn --- class for curve search btn (green bg)
+
+export const CurveBtn = ({ text, cssClass, onClick }) => {
+  return (
+    <button
+      style={{
+        borderRadius: '9% 23% 11% 21% / 26% 57% 34% 60%  ',
+      }}
+      className={cssClass}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
