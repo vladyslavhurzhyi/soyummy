ddc371d63ce404ec5fe4cdb3ba63d468a0336639
Vladyslav Hurzhyi <soulline0@gmail.com>
2023-04-02 13:21:09 +0200

    new addgreen-btn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,11 +13,12 @@ const Home = () => {
       <MainPageTitle pageTitle="Categories" className="main-title-accent" />
       <div className="flex justify-center mt-8">
         <div className="flex bg-hero w-full py-56 justify-center gap-4 bg-cover bg-center bg-no-repeat">
-          <CurveBtn cssClass="registration-btn" text="Registration"></CurveBtn>
-          <CurveBtn cssClass="signin-btn" text="Sign in"></CurveBtn>
           <div>
             <CurveBtn cssClass="addblack-btn" text="Add"></CurveBtn>
+            <CurveBtn cssClass="addbgreen-btn" text="Add"></CurveBtn>
           </div>
+          <CurveBtn cssClass="registration-btn" text="Registration"></CurveBtn>
+          <CurveBtn cssClass="signin-btn" text="Sign in"></CurveBtn>
         </div>
       </div>
       <Loader />
