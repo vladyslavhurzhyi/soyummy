f8abf4510dea90f0a26f42ead7fd955629868e2d
Serhii Malychkovych <masevik@gmail.com>
2023-04-03 19:10:26 +0300

    add shadow to ring

---
+++ src/components/ToggleButton/ToggleButton.jsx
@@ -0,0 +1,9 @@
+export const ToggleButton = () => {
+  return (
+    <div>
+      <div className="relative rounded-full w-[61px] h-[27px] bg-[#EFEFEF] ">
+        <div className="absolute rounded-full top-[3px] left-[2px] w-[21px] h-[21px] bg-gradient-to-b from-[#FFFFFF] to-[#E8EAEA] drop-shadow-[2px_1px_6px_rgba(0,0,0,0.251)]"></div>
+      </div>
+    </div>
+  );
+};
