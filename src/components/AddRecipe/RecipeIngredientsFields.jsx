b3dd31bb5d2ce571a3e332cc44b1dd87b553dd5f
IreneCreadora <nargiza.dancer@gmail.com>
2023-04-04 18:50:51 +0100

    added beta version for ingredient fields

--- src/components/AddRecipe/RecipeIngredientsFields.jsx
+++ src/components/AddRecipe/RecipeIngredientsFields.jsx
@@ -1,7 +1,21 @@
+import { IngredientField } from './IngredientField.jsx';
+
+import { CurveBtn } from 'components/CurveBtn/CurveBtn';
+
 export const ResipeIngredientsFields = () => {
   return (
-    <div className="my-8 font-main font-semibold text-secondaryText text-customBase">
-      Ingredients
+    <div className="my-11 md:my-24">
+      <h2 className="mb-6 md:mb-9 font-main font-semibold text-secondaryText text-customBase">
+        Ingredients
+      </h2>
+      <IngredientField />
+      <CurveBtn
+        text="add ingredient"
+        cssClass="othercateg-btn"
+        onClick={() => {
+          console.log(`Add one more ingr`);
+        }}
+      />
     </div>
   );
 };
