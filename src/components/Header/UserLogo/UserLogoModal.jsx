0df6b7cd4845c2ec077ac13b10093b6fe86f501e
Vladyslav Hurzhyi <80459886+vladyslavhurzhyi@users.noreply.github.com>
2023-04-04 11:37:03 +0200

    Update UserLogoModal.jsx

--- src/components/Header/UserLogo/UserLogoModal.jsx
+++ src/components/Header/UserLogo/UserLogoModal.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from "react";
-import { Modal, Button } from "react-bootstrap";
-import { UserInfoModal } from "./UserInfoModal";
+// import React, { useState } from "react";
+// import { Modal, Button } from "react-bootstrap";
+// import { UserInfoModal } from "./UserInfoModal";
 
-export const UserLogoModal = () => {
-  const [showUserInfoModal, setShowUserInfoModal] = useState(false);
+// export const UserLogoModal = () => {
+//   const [showUserInfoModal, setShowUserInfoModal] = useState(false);
 
-  const handleEditProfile = () => {
-    setShowUserInfoModal(true);
-  };
+//   const handleEditProfile = () => {
+//     setShowUserInfoModal(true);
+//   };
 
-  const handleLogout = () => {
-    // выполнить логаут пользователя
-  };
+//   const handleLogout = () => {
+//     // выполнить логаут пользователя
+//   };
 
-  return (
-    <>
-      <Modal show={showUserInfoModal} onHide={() => setShowUserInfoModal(false)}>
-        <UserInfoModal/>
-      </Modal>
-      <Button variant="primary" onClick={handleEditProfile}>EditProfile</Button>
-      <Button variant="secondary" onClick={handleLogout}>Logout</Button>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Modal show={showUserInfoModal} onHide={() => setShowUserInfoModal(false)}>
+//         <UserInfoModal/>
+//       </Modal>
+//       <Button variant="primary" onClick={handleEditProfile}>EditProfile</Button>
+//       <Button variant="secondary" onClick={handleLogout}>Logout</Button>
+//     </>
+//   );
+// }
