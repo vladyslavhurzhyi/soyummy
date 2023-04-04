2f1bbe470852d3a9a8f7ec9e5f266c887e679043
Vladyslav Hurzhyi <soulline0@gmail.com>
2023-04-04 14:32:18 +0200

    fav item style change

--- src/components/Favorites/FavoriteItem.jsx
+++ src/components/Favorites/FavoriteItem.jsx
@@ -6,8 +6,8 @@ export const FavoriteItem = ({ img, title, description, time }) => {
   return (
     <>
       <li className="flex mx-auto mb-10">
-        <div className=" bg-zinc-50 border-2 border-red-500 w-[1240px]">
-          <div className="flex my-10 mx-10 border-2 border-fuchsia-500">
+        <div className=" bg-zinc-50 w-[1240px] rounded-lg">
+          <div className="flex my-10 mx-10 ">
             <img className="w-[318px] h-[324px] mr-10" src={img} alt={img} />
             <div className="flex flex-col">
               <div className="flex mb-12">
@@ -16,8 +16,10 @@ export const FavoriteItem = ({ img, title, description, time }) => {
                   <Logo />
                 </button>
               </div>
-              <div className="flex h-full justify-between flex-col w-full">
-                <p className="text-lg ">{description}</p>
+              <div className="flex h-full justify-between flex-col">
+                <div className="flex">
+                  <p className="text-lg mr-40 ">{description}</p>
+                </div>
                 <div className="flex justify-between">
                   <p className="mt-auto text-sm font-medium">{time}</p>
                   <CurveBtn cssClass="seerecipe-btn" text="See recipe" />
