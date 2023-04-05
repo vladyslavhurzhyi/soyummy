ffa3551afa4d9762cc775d5c30abf40abb5691df
Vadym-Khokhlov <vadymkhokhlov1989@gmail.com>
2023-04-05 23:58:04 +0300

    Search page without stylization

--- src/components/SearchedRecipesList/SearchedRecipesList.jsx
+++ src/components/SearchedRecipesList/SearchedRecipesList.jsx
@@ -1,24 +1,5 @@
-import { RecipeItem } from '../RecipeItem/RecipeItem';
+import { RecipesList } from 'components/RecipesList/RecipesList';
 
 export const SearchedRecipesList = ({ recipes, cssClass }) => {
-  return (
-    <>
-      <div>
-        <ul className="flex flex-col">
-          {recipes.map(recipe => {
-            return (
-              <RecipeItem
-                key={recipe._id}
-                img={recipe.preview}
-                title={recipe.title}
-                description={recipe.description}
-                time={recipe.time}
-                cssClass={cssClass}
-              ></RecipeItem>
-            );
-          })}
-        </ul>
-      </div>
-    </>
-  );
+  return <RecipesList cssClass={cssClass} data={recipes} />;
 };
