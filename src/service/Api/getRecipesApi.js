3378ac88ee9fad1fcaa683f9501d1018093da98f
Vladyslav Hurzhyi <80459886+vladyslavhurzhyi@users.noreply.github.com>
2023-04-05 23:42:02 +0200

    Update getRecipesApi.js

--- src/service/Api/getRecipesApi.js
+++ src/service/Api/getRecipesApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { token } from 'redux/auth/authOperations';
-const TOKEN =
-  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MmRjYTdlYjZlNTk3NWMwNTQzNDhjZCIsIm5hbWUiOiJnaWFuZnJhbmNvIiwiZW1haWwiOiJ2a2h2MjIwMjJAZ21haWwuY29tIiwiaWF0IjoxNjgwNzIyNTU4LCJleHAiOjE2ODA4MDg5NTh9.ypO9vHmG_RT8XkIZxpZPgHhX-5I1JS1-5ci_BQsAFTQ';
+// const TOKEN =
+//   'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MmRjYTdlYjZlNTk3NWMwNTQzNDhjZCIsIm5hbWUiOiJnaWFuZnJhbmNvIiwiZW1haWwiOiJ2a2h2MjIwMjJAZ21haWwuY29tIiwiaWF0IjoxNjgwNzIyNTU4LCJleHAiOjE2ODA4MDg5NTh9.ypO9vHmG_RT8XkIZxpZPgHhX-5I1JS1-5ci_BQsAFTQ';
 
 axios.defaults.baseURL = 'https://so-yummy-golt.onrender.com/api/v1';
 axios.defaults.params = {
