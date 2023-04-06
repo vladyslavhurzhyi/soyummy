2baa5e0f3226aca7fe97d2f68c9ea13604a60276
Serhii Malychkovych <masevik@gmail.com>
2023-04-06 18:22:38 +0300

    start files

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,5 +1,5 @@
-// import { Footer } from 'components/Footer/Footer';
-// import { Header } from 'components/Header/Header';
+import { Footer } from 'components/Footer/Footer';
+import { Header } from 'components/Header/Header';
 import { Loader } from 'components/Loader/Loader';
 import React from 'react';
 import { Suspense } from 'react';
@@ -8,12 +8,12 @@ import { Outlet } from 'react-router-dom';
 export const SharedLayout = () => {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
 
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
