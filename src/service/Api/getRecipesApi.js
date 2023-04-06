fcc64285942da1f247b29e5a5a3d5aa51a2a1ccf
Serhii Malychkovych <masevik@gmail.com>
2023-04-06 17:56:36 +0300

    fix secret codes

--- src/service/Api/getRecipesApi.js
+++ src/service/Api/getRecipesApi.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 // import { token } from 'redux/auth/authOperations';
-const TOKEN =
-  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MmRjYTdlYjZlNTk3NWMwNTQzNDhjZCIsIm5hbWUiOiJnaWFuZnJhbmNvIiwiZW1haWwiOiJ2a2h2MjIwMjJAZ21haWwuY29tIiwiaWF0IjoxNjgwNzIyNTU4LCJleHAiOjE2ODA4MDg5NTh9.ypO9vHmG_RT8XkIZxpZPgHhX-5I1JS1-5ci_BQsAFTQ';
+// const TOKEN =
+//   'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MmRjYTdlYjZlNTk3NWMwNTQzNDhjZCIsIm5hbWUiOiJnaWFuZnJhbmNvIiwiZW1haWwiOiJ2a2h2MjIwMjJAZ21haWwuY29tIiwiaWF0IjoxNjgwNzIyNTU4LCJleHAiOjE2ODA4MDg5NTh9.ypO9vHmG_RT8XkIZxpZPgHhX-5I1JS1-5ci_BQsAFTQ';
+
+// Секретні дані винесіть у .env. GitHub не пропускає комміти
 
 axios.defaults.baseURL = 'https://so-yummy-golt.onrender.com/api/v1';
 axios.defaults.params = {
@@ -36,7 +38,7 @@ export const getRecipesByQuery = query => {
   return axios
     .get(`/recipes?limit=12&page=1&filter%5Btitle%5D=${query}`, {
       headers: {
-        Authorization: `${TOKEN}`,
+        // Authorization: `${TOKEN}`,
       },
     })
     .then(({ data }) => {
