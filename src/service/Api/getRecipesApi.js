fbab1e5a179c005efd7c7c3739ab445cb37123ab
Vladyslav Hurzhyi <80459886+vladyslavhurzhyi@users.noreply.github.com>
2023-04-06 22:19:58 +0200

    Update getRecipesApi.js

--- src/service/Api/getRecipesApi.js
+++ src/service/Api/getRecipesApi.js
@@ -1,14 +1,6 @@
 import api from './axiosBaseURL';
 
 // import { token } from 'redux/auth/authOperations';
-// const TOKEN =
-//   'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MmRjYTdlYjZlNTk3NWMwNTQzNDhjZCIsIm5hbWUiOiJnaWFuZnJhbmNvIiwiZW1haWwiOiJ2a2h2MjIwMjJAZ21haWwuY29tIiwiaWF0IjoxNjgwNzIyNTU4LCJleHAiOjE2ODA4MDg5NTh9.ypO9vHmG_RT8XkIZxpZPgHhX-5I1JS1-5ci_BQsAFTQ';
-
-// Секретні дані винесіть у .env. GitHub не пропускає комміти
-
-// axios.defaults.params = {
-//   per_page: 8,
-// };
 
 export const getAllRecipes = () => {
   return api
