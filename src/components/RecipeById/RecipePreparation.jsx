7ad766d535eae64359d5495642b84ac5bf1149d8
Serhii Malychkovych <masevik@gmail.com>
2023-04-07 19:11:35 +0300

    pre merge main

--- src/components/RecipeById/RecipePreparation.jsx
+++ src/components/RecipeById/RecipePreparation.jsx
@@ -2,7 +2,9 @@ export const RecipePreparation = () => {
   return (
     <>
       <div className="container">
-        <h2 className="text-customBase">Recipe Preparation</h2>
+        <h2 className="text-customBase text-secondaryText font-semibold mb-[28px]">
+          Recipe Preparation
+        </h2>
       </div>
     </>
   );
