0983877c035fe26d52bfd8ab248e5d976fbceed9
Vladyslav Hurzhyi <80459886+vladyslavhurzhyi@users.noreply.github.com>
2023-04-07 18:03:20 +0200

    Update IngredientsShoppingList.jsx

--- src/components/IngredientsShoppingList/IngredientsShoppingList.jsx
+++ src/components/IngredientsShoppingList/IngredientsShoppingList.jsx
@@ -1,5 +1,5 @@
 // import axios from 'axios';
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 import { motion, Reorder } from 'framer-motion';
 import { staticData } from './staticData';
 import { staticIngredient } from './staticData';
