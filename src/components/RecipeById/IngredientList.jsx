c77fbd1b5257d7c298ad9d8ca517f9f23532b6c7
Serhii Malychkovych <masevik@gmail.com>
2023-04-07 12:02:09 +0300

    pre merge main

--- src/components/RecipeById/IngredientList.jsx
+++ src/components/RecipeById/IngredientList.jsx
@@ -9,7 +9,7 @@ export const IngredientList = data => {
   return (
     <>
       <div className="container">
-        <div className="flex justify-between items-center text-center w-full h-[42px] bg-accentMain dark: rounded-lg font-semibold text-whiteText px-3.5 drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
+        <div className="flex justify-between items-center text-center w-full h-[42px] bg-accentMain rounded-lg font-semibold text-whiteText px-3.5 drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
           <div className="text-customRecipesTime">Ingredients</div>
           <div className="flex">
             <div className="text-customRecipesTime mr-[18px]">Number</div>
