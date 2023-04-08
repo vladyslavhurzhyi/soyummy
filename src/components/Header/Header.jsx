a9840ecdf83f9258d6e1d26938d3dee46d416a02
Vitalii Khymchuk <vitalii.workbox001@gmail.com>
2023-04-09 01:40:59 +0300

    conditional render of UserPanel and reexport

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 import { Navigation } from './Navigation';
 import { UserPanel } from './UserPanel';
 import { ToggleButton } from 'components/ToggleButton/ToggleButton';
+import { useSelector } from 'react-redux';
+import { isLoggedIn } from 'redux/auth/authSelectors';
 export const Header = () => {
+  const shouldShowUser = useSelector(isLoggedIn);
   return (
     <header className="flex mx-10 mt-8 ">
       <Logo className=" " />
       <Navigation />
-      <div className=" mx-20 ">
-        <UserPanel />
-      </div>
+      <div className=" mx-20 ">{shouldShowUser && <UserPanel />}</div>
       <div>
         <ToggleButton />
       </div>
---
+++ src/components/Header/UserPanel/index.js
@@ -0,0 +1,1 @@
+export { UserPanel } from './UserPanel';
