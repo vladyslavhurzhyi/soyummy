234960f6ebf6735dda3dc1da81040f2da5310e4b
Serhii Malychkovych <masevik@gmail.com>
2023-04-08 13:16:16 +0300

    add recipe preparation mob

--- src/components/RecipeById/RecipePreparation.jsx
+++ src/components/RecipeById/RecipePreparation.jsx
@@ -15,29 +15,31 @@ export const RecipePreparation = () => {
           <h2 className="text-customBase text-secondaryText font-semibold mb-[28px]">
             Recipe Preparation
           </h2>
-          <ul className="mb-[50px] text-xs text-[#000000]">
+          <ul className="flex flex-col gap-y-3.5 mb-[50px] text-xs text-[#000000]">
             {instructionsArr.map((step, index) => (
-              <li key={index} className="flex">
-                <div className="rounded-full w-5 h-5 mr-3.5 px-2.5 bg-accentMain">
+              <li key={index} className="flex ">
+                <div className="flex justify-center items-center rounded-full w-5 h-5 mr-3.5 px-2.5 bg-accentMain text-customShoppingList font-semibold text-white">
                   {index + 1}
                 </div>
                 <p>{step}</p>
               </li>
             ))}
           </ul>
-          {{ preview } ? (
-            <img
-              src={preview}
-              className="block  bg-cover bg-center h-[250px] rounded mr-auto"
-              alt="ingredient_photo"
-            />
-          ) : (
-            <img
-              className="block  bg-cover bg-center h-[250px] rounded mr-auto"
-              src={placeholder}
-              alt="placeholder"
-            />
-          )}
+          <div className="w-[343px] h-[250px] overflow-auto">
+            {{ preview } ? (
+              <img
+                src={preview}
+                className="block   w-full rounded mr-auto"
+                alt="ingredient_photo"
+              />
+            ) : (
+              <img
+                className="block  bg-cover bg-center w-full rounded mr-auto"
+                src={placeholder}
+                alt="placeholder"
+              />
+            )}
+          </div>
         </div>
       </div>
     </>
