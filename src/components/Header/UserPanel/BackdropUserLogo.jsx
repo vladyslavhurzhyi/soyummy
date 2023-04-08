6116f41b51f35ceca020ca157afc5d6cbb133865
Vitalii Khymchuk <vitalii.workbox001@gmail.com>
2023-04-09 01:39:13 +0300

    deleted old files

--- src/components/Header/UserPanel/BackdropUserLogo.jsx
+++
@@ -1,8 +0,0 @@
-export const BackdropUserLogo = ({ onClick, className, children }) => {
-  return (
-    <div className={`flex items-center justify-center ${className}`} onClick={onClick}>
-      {children}
-    </div>
-  );
-};
-
