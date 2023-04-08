fdaea7056c409bbce2a6d34d1d8fecd081c1fa5a
Serhii Malychkovych <masevik@gmail.com>
2023-04-09 00:50:31 +0300

    pre merge

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -8,13 +8,13 @@ import { Outlet } from 'react-router-dom';
 export const SharedLayout = () => {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <Suspense fallback={<Loader />}>
         <div className="relative ">
           <Outlet />
         </div>
       </Suspense>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
