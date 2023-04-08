029bdd543702d6d0769f44633c4e4f5ab675c925
SergeyPochapskiy <104765308+SergeyPochapskiy@users.noreply.github.com>
2023-04-09 00:52:23 +0300

    fix header style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,15 +5,19 @@ import { UserLogo } from './UserLogo/UserLogo';
 import { ToggleButton } from "components/ToggleButton/ToggleButton"
 export const Header = () => {
   return (
-    <header className="flex mx-10 mt-8 ">
-      <Logo className=" "/>
-        <Navigation/>
-        <div className=" mx-20 "> 
-          <UserLogo/>
-        </div>
-        <div>
-          <ToggleButton/>  
-        </div>
+    <header className="absolute w-full z-10">
+      {/* <div class="mx-auto px-4 sm:px-8 xl:px-100 max-w-sm sm:max-w-md lg:max-w-4xl"> */}
+        <div class="flex justify-between items-center h-auto pt-18 lg:pt-19 xl:pt-18">
+            <Logo className=" "/>
+            <Navigation/>
+            <div className="  "> 
+              <UserLogo/>
+            </div>
+            <div>
+              <ToggleButton/>  
+            </div>
+          </div>
+      {/* </div> */}
     </header>
   );
 };
