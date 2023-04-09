5749d30828f3bc00b482aa77c2b64e820156942f
Serhii Malychkovych <masevik@gmail.com>
2023-04-09 23:34:13 +0300

    add logic for checkbox

--- src/components/RecipeById/IngredientList.jsx
+++ src/components/RecipeById/IngredientList.jsx
@@ -5,10 +5,17 @@ import {
   addShoppingList,
   deleteShoppingList,
 } from 'redux/shoppingList/shoppingListOperations';
+import {
+  selectError,
+  selectIsLoading,
+} from 'redux/shoppingList/shoppingListSelectors';
 
 export const IngredientList = () => {
   const dispatch = useDispatch();
   const recipes = useSelector(selectRecipes);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
   const { _id: recipeId, ingredients } = recipes;
 
   const handleChange = event => {
@@ -63,19 +70,24 @@ export const IngredientList = () => {
                 data-ingrmeasure={measure}
                 className="appearance-none text-accentMain cursor-pointer w-[18px] md:w-[35px] h-[18px] md:h-[35px] rounded bg-transparent border-solid border-opacity-50 checked:opacity-0 border-[#7E7E7E] transition-all duration-300 peer"
               />
-              <div className="absolute flex justify-center items-center w-[18px] md:w-[35px] h-[18px] md:h-[35px] rounded pointer-events-none bg-transparent opacity-0 border border-solid border-opacity-50 border-[#7E7E7E] transition-all duration-300 peer-checked:opacity-100">
-                <svg
-                  viewBox="0 0 10 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-2.5 md:w-5 h-2.5 md:h-5"
-                >
-                  <path
-                    d="M7.77377 1.01229C8.04472 0.612637 8.58836 0.508303 8.98801 0.779256C9.38767 1.05021 9.492 1.59384 9.22105 1.9935L4.47925 8.98765C4.13243 9.4992 3.37879 9.4992 3.03197 8.98765L0.779622 5.66544C0.508669 5.26578 0.613003 4.72215 1.01266 4.45119C1.41231 4.18024 1.95595 4.28457 2.2269 4.68423L3.75561 6.93907L7.77377 1.01229Z"
-                    fill="#8BAA36"
-                  />
-                </svg>
-              </div>
+              {!error && (
+                <div className="absolute flex justify-center items-center w-[18px] md:w-[35px] h-[18px] md:h-[35px] rounded pointer-events-none bg-transparent opacity-0 border border-solid border-opacity-50 border-[#7E7E7E] transition-all duration-300 peer-checked:opacity-100">
+                  <svg
+                    viewBox="0 0 10 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-2.5 md:w-5 h-2.5 md:h-5"
+                  >
+                    <path
+                      d="M7.77377 1.01229C8.04472 0.612637 8.58836 0.508303 8.98801 0.779256C9.38767 1.05021 9.492 1.59384 9.22105 1.9935L4.47925 8.98765C4.13243 9.4992 3.37879 9.4992 3.03197 8.98765L0.779622 5.66544C0.508669 5.26578 0.613003 4.72215 1.01266 4.45119C1.41231 4.18024 1.95595 4.28457 2.2269 4.68423L3.75561 6.93907L7.77377 1.01229Z"
+                      fill="#8BAA36"
+                    />
+                  </svg>
+                </div>
+              )}
+              {error && (
+                <div className="absolute flex justify-center items-center w-[18px] md:w-[35px] h-[18px] md:h-[35px] rounded pointer-events-none bg-transparent opacity-0 border border-solid border-opacity-50 border-[#7E7E7E] transition-all duration-300 peer-checked:opacity-100"></div>
+              )}
             </div>
           </div>
         </li>
---
+++ src/redux/shoppingList/shoppingListSelectors.js
@@ -0,0 +1,3 @@
+export const selectRecipes = state => state.shoppingList.items;
+export const selectIsLoading = state => state.shoppingList.isLoading;
+export const selectError = state => state.shoppingList.error;
