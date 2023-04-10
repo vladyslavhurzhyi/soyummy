265221d38936959b9f7f65db720d52af45701d25
IreneCreadora <nargiza.dancer@gmail.com>
2023-04-10 13:10:22 +0100

    add path for popular recipes

--- src/components/PopularRecipes/PopularRecipes.jsx
+++ src/components/PopularRecipes/PopularRecipes.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 export const PopularRecipes = ({ data }) => {
   return (
     <div className="relative md:w-full lg:w-80 lg:absolute lg:top-[500px] lg:left-[900px] xl:left-[1056px]">
@@ -10,19 +12,21 @@ export const PopularRecipes = ({ data }) => {
             key={_id}
             className="flex md:w-1/2 lg:w-full border-b-2 border-gray-300 pb-3.5 pt-6 pr-2 dark:border-gray-500"
           >
-            <img
-              className="w-[104px] h-[85px] rounded-md mr-3"
-              src={thumb}
-              alt={title}
-            />
-            <div className="flex flex-col">
-              <p className="text-customSm text-secondaryText font-medium dark:text-whiteText">
-                {title}
-              </p>
-              <p className="text-xs text-gray-500 font-normal dark:text-gray-400">
-                {description}
-              </p>
-            </div>
+            <Link to={`/recipe/${_id}`} className="flex">
+              <img
+                className="w-[104px] h-[85px] rounded-md mr-3"
+                src={thumb}
+                alt={title}
+              />
+              <div className="flex flex-col">
+                <p className="text-customSm text-secondaryText font-medium dark:text-whiteText">
+                  {title}
+                </p>
+                <p className="text-xs text-gray-500 font-normal dark:text-gray-400">
+                  {description}
+                </p>
+              </div>
+            </Link>
           </li>
         ))}
       </ul>
