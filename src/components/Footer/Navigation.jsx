0accb3be6f670a31e52af4dcc769040d64827c5f
Olena Bondiuchna <93439766+elenbond@users.noreply.github.com>
2023-04-11 01:37:20 +0300

    fixed styles 1

--- src/components/Footer/Navigation.jsx
+++ src/components/Footer/Navigation.jsx
@@ -8,34 +8,32 @@ export const Navigation = () => {
     // const {isLoggedIn} = useAuth();
 
     return (
-        <nav>
-            <div className='md:w-full justify-center justify-items-center justify-self-center'>
-                <div className='flex flex-col xl:flex-row'
+        <nav className='flex justify-center lg:justify-between '>
+            <div className='md:w-full justify-center justify-items-center justify-self-center md:pl-[62px] lg:pl-0 content-start'>
+                <div className='flex flex-col lg:flex-row lg:items-start lg:min-w-[500px] xl:max-w-[687px]'
                     style={{
-                        display: 'flex',
-                        width: '100%',
-                    alignItems: 'flex-start',
+                        // display: 'flex',
+                        // width: '100%',
+                    // alignItems: 'flex-start',
                     justifyContent: 'space-between',
                 }}>
-                    <div className='flex flex-col justify-center items-center'
-                        // style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}
-                    >
-                        <div className="block md:hidden xl:block">
-                        <Nav /> 
+                    <div className='flex flex-col justify-center items-center'>
+                        <div className="block md:hidden lg:block">
+                            <Nav /> 
                         </div>
                         
-                        <div className="hidden xl:block xl:mt-24">
+                        <div className="hidden lg:block lg:mt-24">
                             <FollowUs />
                         </div>
                     </div>
-                    <div className='block md:hidden xl:block'>
+                    <div className='block md:hidden lg:block'>
                         <SubscribeForm/>
                     </div>
                 </div>
-                <div className='hidden md:block xl:hidden'>
+                <div className='hidden md:block md:justify-self-center lg:hidden'>
                     <SubscribeForm/>
                 </div>
-                <div className="block xl:hidden mt-[44px] xl:mt-24">
+                <div className="block lg:hidden mt-[44px] lg:mt-24">
                     <FollowUs />
                 </div>
             </div>
