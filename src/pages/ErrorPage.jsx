d96f7a10db33864dba84108a0db078039f8b9f40
Serhii Malychkovych <masevik@gmail.com>
2023-04-11 21:24:26 +0300

    add tab style

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -16,12 +16,12 @@ const ErrorPage = () => {
           <img
             src={error}
             alt="error"
-            className="rounded mb-3.5 md:mb-8 pt-4 md:h-[327px]"
+            className="rounded mb-3.5 md:mb-8 pt-4 md:h-[327px] xl:h-[331px]"
           />
           <p className="text-[18px] md:text-customBase font-semibold mb-2 md:mb-3.5">
             We are sorry,
           </p>
-          <p className="text-customXxs md:text-[18px] leading-[18px] mb-[100px] md:mb-[204px] max-w-48 md:max-w-full md:min-w-[430px]">
+          <p className="text-customXxs md:text-[18px] leading-[18px] mb-[100px] md:mb-[204px] w-48 md:w-full md:min-w-[430px]">
             but the page you were looking for can’t be found..
           </p>
         </div>
