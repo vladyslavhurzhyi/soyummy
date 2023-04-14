4701e4d3b80670d513ea10072e893742abe4544c
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-04-14 15:17:52 +0300

    fix notification

--- src/components/IngredientsShoppingList/PopUpDraggable.jsx
+++ src/components/IngredientsShoppingList/PopUpDraggable.jsx
@@ -5,8 +5,10 @@ export const PopUpDraggable = () => {
     <motion.div
       initial={{
         opacity: 1,
+        scale: 0,
+        rotate: -15,
       }}
-      animate={{ opacity: 0 }}
+      animate={{ opacity: 0, scale: 3, rotate: -15 }}
       transition={{
         delay: 5,
         duration: 2,
@@ -15,7 +17,7 @@ export const PopUpDraggable = () => {
         repeatType: 'loop',
         repeatDelay: 5,
       }}
-      className="absolute top-3 rotate-[20deg] animate-ping text-customRecipesText text-accentMain"
+      className="absolute top-5 text-customRecipesText text-accentMain"
     >
       <p>Drag Me</p>
     </motion.div>
